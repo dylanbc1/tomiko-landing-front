53bb71b9466cf4e44f4a9419cfa7cf95c0f468c9
dylanbc1 <dyez1110@gmail.com>
2025-01-09 15:34:06 -0500

    feat: Change eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Desactiva el error para variables declaradas pero no utilizadas
+      "no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
